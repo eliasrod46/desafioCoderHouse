7fd7fdfd9a789dcf10c84e8e74d604c8f2c368c2
Elias Rodriguez <eliasrod46@gmail.com>
2022-10-10 17:38:15 -0300

    inicio desafio 13

--- index.js
+++
@@ -1,7 +0,0 @@
-const { serverHttp } = require("./app.js");
-
-//start Server
-const port = 8080;
-const server = serverHttp.listen(port, () => {
-  console.log(`The server is listening in port: ${port}`);
-});
--- routes/fakeProducts.routes.js
+++
@@ -1,7 +0,0 @@
-//Requires
-const { Router } = require("express");
-const productFakeController = require("../controllers/productFake.controller.js");
-const router = Router();
-
-router.get("/", productFakeController.getData);
-module.exports = router;
