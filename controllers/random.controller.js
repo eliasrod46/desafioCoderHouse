1b0e6a34e8ada64122b8c3466ab88bcec74d2663
Elias Rodriguez <eliasrod46@gmail.com>
2022-10-13 13:48:19 -0300

    1310221348

--- controllers/random.controller.js
+++ controllers/random.controller.js
@@ -1,10 +1,19 @@
 const { fork } = require("child_process");
 
 const getData = async (req, res) => {
-  const random = req.query;
-  let calculorandmon = fork("../utils/forkRandom.js");
-  calculorandmon.send("start");
-  res.send(random);
+  let cant;
+  //Verifico que reciba la query cant y si esta dentro del trango pedido
+  if (req.query.cant && req.query.cant >= 1 && req.query.cant <= 1000000) {
+    cant = req.query.cant;
+  } else {
+    cant = 100000000;
+  }
+  let calculorandmon = fork("./utils/forkRandom.js");
+  calculorandmon.send({ msg: "start", data: cant });
+
+  calculorandmon.on("message", (msg) => {
+    res.send(msg);
+  });
 };
 
 module.exports = { getData };
--- utils/forkRandom.js
+++ utils/forkRandom.js
@@ -1,5 +1,17 @@
-process.on("message", (msg) => {
-  if (msg == "start") {
-    process.send({ type: "sum", data: sum });
+//El dato devuelto al frontend será un objeto que contendrá como claves los números random generados junto a la cantidad de veces que salió cada uno. Esta ruta no será bloqueante (utilizar el método fork de child process). Comprobar el no bloqueo con una cantidad de 500.000.000 de randoms.
+
+process.on("message", (info) => {
+  if (info.msg == "start") {
+    const respuesta = {};
+    for (let i = 0; i < info.data; i++) {
+      let num = Math.floor(Math.random() * info.data);
+      if (!respuesta[num]) {
+        respuesta[num] = 1;
+      } else {
+        respuesta[num]++;
+      }
+    }
+
+    process.send({ respuesta });
   }
 });
